21a706a38bbcb374f191fd8354e6f40588983f99
Metin97 <74401058+Meto187@users.noreply.github.com>
2021-02-28 00:52:36 +0100

    Update frmBenutzerStartSeite.cs

--- frmBenutzerStartSeite.cs
+++ frmBenutzerStartSeite.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BenutzerGenerator_MetinDereli
+namespace BenutzerGenerator_MetinDerelii
 {
     public partial class FrmBenutzerGenerator : Form
     {
